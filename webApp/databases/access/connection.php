c7066eeb3402b59450ec5876592b31004563d0f0
Ondrej Planer <oplaner4@gmail.com>
2023-02-05 10:47:02 +0100

    connection php

--- webApp/databases/access/connection.php
+++ webApp/databases/access/connection.php
@@ -3,7 +3,7 @@
         $servername = "localhost";
         $username = "root";
         $password = "";
-        $dbname = "spotGame";
+        $dbname = "spotgame";
 
         $conn = new mysqli($servername, $username, $password, $dbname);
         mysqli_set_charset($conn,"utf8");
